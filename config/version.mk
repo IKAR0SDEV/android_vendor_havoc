93262ab357dc9d6ab168c948c3244e867f830bc0
IkarosDev <44556469+IKAR0SDEV@users.noreply.github.com>
2020-04-10 23:57:24 +0100

    Update version.mk

--- config/version.mk
+++ config/version.mk
@@ -10,6 +10,6 @@ TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
 # Set all versions
 HAVOC_VERSION = 3.6
-HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y-%m%s)
+HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y-%H%M)
 HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
-HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d-%m%s)/$(shell date -u +%H%M-%m%s)
+HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d-%H%M)/$(shell date -u +%H%M-%H%M)
