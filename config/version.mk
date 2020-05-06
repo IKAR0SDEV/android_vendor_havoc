583b542e6b18ac9d8a3a3ebbc1e081c2225c30f9
Anushek Prasal <anushekprasal@gmail.com>
2020-05-06 13:40:41 +0530

    vendor: Havoc 3.5
    Wallpaper by faaiq ackmerd. Added from https://www.pexels.com/photo/photo-of-night-sky-1025469/

--- config/version.mk
+++ config/version.mk
@@ -9,7 +9,7 @@ endif
 TARGET_PRODUCT_SHORT := $(subst havoc_,,$(HAVOC_BUILD_TYPE))
 
 # Set all versions
-HAVOC_VERSION = 3.4
+HAVOC_VERSION = 3.5
 HAVOC_BUILD_DATE := $(shell date -u +%d-%m-%Y-%H%M)
 HAVOC_BUILD_VERSION := Havoc-OS-v$(HAVOC_VERSION)-$(shell date -u +%Y%m%d-%H%M)-$(HAVOC_BUILD)-$(HAVOC_BUILD_TYPE)
 HAVOC_FINGERPRINT := Havoc-OS/v$(HAVOC_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d-%H%M)/$(shell date -u +%H%M-%H%M)
